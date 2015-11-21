cb34162de51f36e2d6b846cfbb6e9d6fe8801e48
Andres Perez <andres15perez@gmail.com>
2015-11-21 15:56:56 -0500

    implement send message

--- client/client.py
+++ client/client.py
@@ -1,7 +1,10 @@
 class OTC_Client(object):
     def __init__(self):
+
         raise NotImplementedError("TODO: write a client")
     def send(message):
+        payload = {'message':message}
+        r = requets.post(self.server_address,data=payload)
         raise NotImplementedError("TODO: write send method")
     def recieve():
         raise NotImplementedError("TODO: write recieve")
@@ -9,7 +12,8 @@ class OTC_Client(object):
         raise NotImplementedError("TODO: clients need to decrypt messages")
     def encrypt(encrypt):
         raise NotImplementedError("TODO: clients need to encrypt messages")
-    def connect():
+    def connect(server_address):
+        self.server_address = server_address
         raise NotImplementedError("TODO:clients need to be able to connect to server")
 if __name__ == "__main__":
     client = OTC_Client()
