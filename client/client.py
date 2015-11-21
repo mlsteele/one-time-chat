677ff2af66a3f340138d709e57123a7c4dad3410
Andres Perez <andres15perez@gmail.com>
2015-11-21 15:57:48 -0500

    correct client specs

--- client/client.py
+++ client/client.py
@@ -2,17 +2,17 @@ class OTC_Client(object):
     def __init__(self):
 
         raise NotImplementedError("TODO: write a client")
-    def send(message):
+    def send(self,message):
         payload = {'message':message}
         r = requets.post(self.server_address,data=payload)
         raise NotImplementedError("TODO: write send method")
-    def recieve():
+    def recieve(self):
         raise NotImplementedError("TODO: write recieve")
-    def decrypt(message, pad_index=current_index):
+    def decrypt(self,message, pad_index=current_index):
         raise NotImplementedError("TODO: clients need to decrypt messages")
-    def encrypt(encrypt):
+    def encrypt(self,encrypt):
         raise NotImplementedError("TODO: clients need to encrypt messages")
-    def connect(server_address):
+    def connect(self,server_address):
         self.server_address = server_address
         raise NotImplementedError("TODO:clients need to be able to connect to server")
 if __name__ == "__main__":
