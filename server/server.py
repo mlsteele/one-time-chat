4763194ddfec9d24d05295e805e4735da6324984
Miles Steele <miles@milessteele.com>
2015-11-21 17:08:15 -0500

    add getmessages

--- server/server.py
+++ server/server.py
@@ -11,7 +11,7 @@ class APIException(Exception):
 
     def to_dict(self):
         return {
-            "status": self.status_code,
+            "error": self.status_code,
             "message": self.message,
         }
 
@@ -23,13 +23,18 @@ def handle_invalid_usage(error):
 
 @app.route("/")
 def home():
-    return "otc server"
+    return "One Time Chat API Server"
 
 @app.route("/send", methods=["GET", "POST"])
 def send():
+    """
+    Send a message to a user.
+    Places the message in that users's mailbox.
+    """
     if request.method != "POST":
         raise APIException(400, "Message send requests must be HTTP POST.")
 
+    # TODO(miles): this will actually be a form in a sec.
     message = request.get_data()
 
     # Echo tail is the last 10 bytes of the received message.
@@ -39,6 +44,40 @@ def send():
     resdict = {
         "received": True,
         "echo_tail": echo_tail,
+    }
+    return jsonify(resdict)
+
+@app.route("/getmessages", methods=["GET"])
+def getmessages():
+    """
+    Get all messages destined for a certain user.
+    Only returns messages since a certain message reference number.
+    """
+    # ID of the user receiving the message.
+    uid = request.args.get("uid")
+    # The reference number of the last-seen message.
+    last_seen = request.args.get("last_seen")
+
+    if not uid:
+        raise APIException(400, "getmessages missing required field: uid")
+    if not last_seen:
+        raise APIException(400, "getmessages missing required field: last_seen")
+
+    # TODO(miles): remove dummy data
+    messages = ["hi", "there"]
+    last_message_counter = 5
+
+    resdict = {
+        # ID of the user the messages were sent to.
+        "receiver_uid": uid,
+        # How many messages in this response.
+        "count": len(messages),
+        # Array of messages.
+        "messages": messages,
+        # Whether this response includes all known messages after last_seen.
+        "complete": True,
+        # The reference number of the last message shown.
+        "last_ref": last_message_counter,
     }
     return jsonify(resdict)
 
