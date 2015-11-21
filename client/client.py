7b97e19b718a9d009d0e5c4f40224c75ea9730bd
Andres Perez <andres15perez@gmail.com>
2015-11-21 15:49:11 -0500

    add specs to client

--- client/client.py
+++ client/client.py
@@ -1,6 +1,15 @@
 class OTC_Client(object):
     def __init__(self):
         raise NotImplementedError("TODO: write a client")
-
+    def send(message):
+        raise NotImplementedError("TODO: write send method")
+    def recieve():
+        raise NotImplementedError("TODO: write recieve")
+    def decrypt(message, pad_index=current_index):
+        raise NotImplementedError("TODO: clients need to decrypt messages")
+    def encrypt(encrypt):
+        raise NotImplementedError("TODO: clients need to encrypt messages")
+    def connect():
+        raise NotImplementedError("TODO:clients need to be able to connect to server")
 if __name__ == "__main__":
     client = OTC_Client()
--- server/server.py
+++ server/server.py
@@ -1,6 +1,6 @@
 class OTC_Server(object):
     def __init__(self):
         raise NotImplementedError("TODO: write a server")
-
+    def 
 if __name__ == "__main__":
     server = OTC_Server()
