6f45d1fe884b4700e5583d3c1c4861d0ea9f3ce4
Miles Steele <miles@milessteele.com>
2015-11-23 10:38:42 -0500

    update client communication with server

--- client/client.py
+++ client/client.py
@@ -4,24 +4,24 @@ import sys
 
 class OTC_Client(object):
     # A client is initialized with the address of the server it intends to
-    # connect to
+    # connect to.
 
-    # TODO: fix the initialization with pad. Client shouldn't get access to pad
-    def __init__(self, server_address, device_id, username=None,):
-        self.encrypt_index = 0
-        self.device_id = device_id
-        # TODO: what is a good value for the decrypt index?
-        self.decrypt_index = None
-        if username is None:
-            self.username = self.getUserId()
-        else:
-            self.username = username
+    def __init__(self, server_address, device_address):
+        if not server_address.startswith("http://"):
+            server_address = "http://" + server_address
         self.server_address = server_address
+        self.user_id = self.get_user_id()
         self.connect()
 
     def send(self, message, target):
-        payload = {'message': message, 'target': target}
-        r = requests.post(self.server_address, data=payload)
+        payload = {
+            "recipient_uid": target,
+            "sender_uid": self.user_id,
+            "contents": message,
+        }
+        res = requests.post(self.server_address + "/send", data=payload)
+        # TODO: handle server errors
+        assert res.status_code == 200
 
     def recieve(self):
         raise NotImplementedError("TODO: write recieve")
@@ -34,25 +34,30 @@ class OTC_Client(object):
         raise NotImplementedError("TODO: clients need to encrypt messages")
 
     def connect(self):
-        payload = {'message': "connect"}
-        r = requests.post(self.server_address, data=payload)
-        # TODO: handle response from server
+        res = requests.get(self.server_address + "/check")
+        # TODO: handle server errors
+        assert res.status_code == 200
 
     def readFromDevice(self):
         raise NotImplementedError(
             "TODO: clients need to be able to read from their device")
-    # TODO: http form maybe
+            # TODO: http form maybe
 
-    def getUserId(self):
-        raise NotImplementedError(
-            "TODO: clients need to be able to fetch user id from their device")
+    def get_user_id(self):
+        print "TODO: clients need to be able to fetch user id from their device"
+        return raw_input("enter a fake user id: ")
 
-    def getMessages(self, name, cursor):
-        payload = {'uid': name, 'last_seen': cursor}
-        r = requests.post(self.server_address + "/getmessages", params=payload)
-        return r
-        raise NotImplementedError(
-            "TODO: clients need to be able to poll messages")
+    def get_messages(self, start_ref):
+        """Get all messages starting at start_ref."""
+        payload = {
+            "recipient_uid": self.user_id,
+            "start_ref": start_ref,
+            # maxcount optional
+        }
+        res = requests.get(self.server_address + "/getmessages", params=payload)
+        # TODO: handle server errors
+        assert res.status_code == 200
+        return res.json()
 
     def run(self):
         cursor = 0
@@ -62,12 +67,14 @@ class OTC_Client(object):
             self.send(message, target)
             print "Message sent!"
             # GET messages
-            response = self.getMessages(self.username, cursor)
+            response = self.getMessages(self.user_id, cursor)
 
 if __name__ == "__main__":
     if (len(sys.argv) < 3):
-        print "ERROR: correct usage is client.py [server_address] [pad_file]"
+        print "ERROR: correct usage is client.py [server_address] [device_address]"
+        sys.exit(-1)
     server_address = sys.argv[1]
-    pad_file = sys.argv[2]
-    client = OTC_Client(server_address, device_id)
-    client.send("HELLO", "alice")
+    device_address = sys.argv[2]
+    client = OTC_Client(server_address, device_address)
+    client.send("HELLO", "bob")
+    print client.get_messages(0)
