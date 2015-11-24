b694b2b54605b76be84e15bda448b3cf2f6e7bba
Andres Perez <andres15perez@gmail.com>
2015-11-23 22:32:06 -0500

    better get in run

--- client/client.py
+++ client/client.py
@@ -99,6 +99,7 @@ if __name__ == "__main__":
     bob = OTC_Client(server_address, "bob")
     print "Sending 'HELLO' to alice"
     alice.send("HELLO", "bob")
+    alice.send("how are you?", "bob")
     messages = bob.get_messages(0)[u'messages']
     for message in messages:
         print message[u'sender_uid'] + ":" + message[u'contents']
