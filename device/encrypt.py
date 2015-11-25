27d7db87ec425d8f593b288b0a4daefbf865c36c
Andres Perez <andres15perez@gmail.com>
2015-11-24 19:44:26 -0500

    add encryption to client

--- device/encrypt.py
+++ device/encrypt.py
@@ -5,6 +5,15 @@ def xor_string(a, b):
         raise ValueError("Messages must be the same length!")
     return a ^ b
 
+def xor(charA, charB):
+    return chr(ord(charA) ^ ord(charB))
+def xorHelper((charA,charB)):
+    return xor(charA,charB)
+def encrypt(message,pad):
+    return map(xorHelper,zip(message,pad))
+def decrypt(cipher,pad):
+    return map(xorHelper,zip(cipher,pad))
+
 # For now let's just do
 # i || p2 xor ((p1 xor m) || MAC(p1 xor m))
 # j || pj xor (i || pi xor m || MAC(i || pi xor m))
