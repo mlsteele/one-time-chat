16a037e38dfdf13809196651d8162281c0045016
Andres Perez <andres15perez@gmail.com>
2015-11-26 15:30:53 -0500

    add encrypt to rpcmethods.py

--- device/rpcmethods.py
+++ device/rpcmethods.py
@@ -1,8 +1,24 @@
+import encrypt
 """
 All methods that are RPCs should go here.
 """
 
 def encrypt(recipient_uid, message):
+    """ Encrypts a message using a one time pad  
+    recipient_uid is the id of the recipient. This impacts what pad will be used to encrypt
+    message is the intended message to be encrypted
+    returns a dictionary with keys cipher_text, and index_used
+    """
+    ## I imagine the pad to be read_from_device depends on the recipient_uid, the index, and the length of the pad
+    return 0
+    (pad,index_used) = read_encryption_pad(recipient_uid,len(message)) 
+    cipher_list = encrypt.encrypt(message,pad)
+    cipher_text = encrypt.pretty_print(cipher_list)
+    return {
+            "status":"ok",
+            "cipher_text":cipher_text,
+            "index_used":index_used,
+    }
     return {
         "status": "failed",
         "reason": "I don't know how to encrypt",
